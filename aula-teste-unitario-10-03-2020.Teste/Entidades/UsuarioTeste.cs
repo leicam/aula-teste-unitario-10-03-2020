f12d5e81cf2cdfa48384d1d99bba0a22205fb741
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2020-03-11 20:41:31 -0300

    Teste unitario

---
+++ aula-teste-unitario-10-03-2020.Teste/Entidades/UsuarioTeste.cs
@@ -0,0 +1,27 @@
+﻿using aula_testes_unitarios_10_03_2020.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_teste_unitario_10_03_2020.Teste.Entidades
+{
+    [TestClass]
+    public class UsuarioTeste
+    {
+        private const string _owner = "Juliano";
+        private const string _category = "Usuario";
+
+        [TestMethod]
+        [Owner(_owner)]
+        [TestCategory(_category)]
+        public void Usuario_Instancia_Sucesso()
+        {
+            var usuario = new Usuario("Teste", "Teste");
+
+            Assert.IsNotNull(usuario);
+        }
+    }
+}
---
+++ aula-teste-unitario-10-03-2020.Teste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("aula-teste-unitario-10-03-2020.Teste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aula-teste-unitario-10-03-2020.Teste")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9e36a70-8309-4439-be52-35d21cf64ee5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
